80ff4332839a14bcf0434753fd2fe11b24b8fa97
net2cn <mcopener@gmail.com>
2020-06-06 20:24:51 +0800

    Optimize vectorizer.
    Original implementation is way too slow.
    
    Signed-off-by: net2cn <mcopener@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import numpy as np
+import scipy.sparse as sp
+import array
 import pickle
 import re
 import os
-import multiprocessing
+import tqdm
 
 def read_data(path):
     # PhraseId SentenceId Phrase Sentiment
@@ -25,23 +27,21 @@ def read_data(path):
     return data
 
 class CountVectorizer():
-    def __init__(self, sentences, stopwords):
+    def __init__(self, sentences, stopwords, dtype=np.int64):
         self.sentences = sentences
         self.stopwords = stopwords
+        self.dtype = dtype
 
-        self.vocabulary = []
+        self.vocabulary = {}
         self.bow = []
-
-        self.vocabulary = self.tokenize(self.sentences)
-        self.bow = self.generate_bow(self.sentences, self.vocabulary)
         
     def tokenize(self, sentences):
         words = []
-        for sentence in sentences:
+        for sentence in tqdm.tqdm(sentences, total=len(sentences), ascii=True):
             w = self.word_extraction(sentence)
             words.extend(w)
         
-        words = sorted(list(set(words)))
+        words = {k: v for v, k in enumerate(sorted(list(set(words))))}
         return words
 
     def word_extraction(self, sentence):
@@ -49,20 +49,57 @@ class CountVectorizer():
         cleaned_text = [w.lower() for w in words if w not in self.stopwords]
         return cleaned_text    
         
-    def generate_bow(self, sentences, vocab):
-        bow = []
+    def vectorize(self, sentences, vocab):
+        j_indices = []
+        indptr = []
 
-        for sentence in sentences:
+        values = array.array(str("i"))
+        indptr.append(0)
+
+        # Partially from scipy. For significant speed-up.
+        for sentence in tqdm.tqdm(sentences, total=len(sentences), ascii=True):
             words = self.word_extraction(sentence)
-            bag_vector = np.zeros(len(vocab))
-            for w in words:
-                for i,word in enumerate(vocab):
-                    if word == w: 
-                        bag_vector[i] += 1
+            feature_counter = {}
+            for word in words:
+                try:
+                    feature_idx = vocab[word]
+                    if feature_idx not in feature_counter:
+                        feature_counter[feature_idx] = 1
+                    else:
+                        feature_counter[feature_idx] += 1
+                except KeyError:
+                    continue
             
-            bow.append(np.array(bag_vector))
+            j_indices.extend(feature_counter.keys())
+            values.extend(feature_counter.values())
+            indptr.append(len(j_indices))
 
-        return bow
+        if indptr[-1] > np.iinfo(np.int32).max:  # = 2**31 - 1
+            if _IS_32BIT:
+                raise ValueError(('sparse CSR array has {} non-zero '
+                                'elements and requires 64 bit indexing, '
+                                'which is unsupported with 32 bit Python.')
+                                .format(indptr[-1]))
+                indices_dtype = np.int64
+        else:
+            indices_dtype = np.int32
+
+        j_indices = np.asarray(j_indices, dtype=indices_dtype)
+        indptr = np.asarray(indptr, dtype=indices_dtype)
+        values = np.frombuffer(values, dtype=np.intc)
+
+        # Using dense matrix only causes unaccpetable large pkl file.
+        X = sp.csr_matrix((values, j_indices, indptr),
+                        shape=(len(indptr) - 1, len(vocab)),
+                        dtype=self.dtype)
+        X.sort_indices()
+        return X
+
+    def run(self):
+        print("Calculating vocabulary...")
+        self.vocabulary = self.tokenize(self.sentences)
+        print("Making BoW...")
+        self.bow = self.vectorize(self.sentences, self.vocabulary)
 
 def LogisticRegressor():
     def __init__(x, y):
@@ -87,18 +124,17 @@ def main():
     test_dataset = read_data(test_dataset_path)[1:]
     stopwords = sum(read_data(stopwords_path), [])  # Flatten stopwords
 
-    train_vectorizer = CountVectorizer([train_dataset[i][2] for i in range(len(train_dataset))][0:50], stopwords)
-    print(*train_vectorizer.bow, sep="\n")
+    if not os.path.exists("./data/vectorizer.pkl"):
+        print("Preparing data... This may take a while...")
+        train_vectorizer = CountVectorizer([train_dataset[i][2] for i in range(len(train_dataset))], stopwords)
+        train_vectorizer.run()
+        os.mkdir("./data")
+        pickle.dump(train_vectorizer, open("./data/vectorizer.pkl", "wb"))
 
-    # if not os.path.exists("./data/train.pkl"):
-    #     print("Preparing data... This may take a while...")
-    #     train_vectorizer = CountVectorizer([train_dataset[i][2] for i in range(len(train_dataset))][0:50], stopwords)
-    #     pickle.dump(train_vectorizer)
+    train_vectorizer = pickle.load(open("./data/vectorizer.pkl", "rb"))
 
-    # train_vectorizer = pickle.load("./data/train.pkl")
-
-    # x_train = train_vectorizer.bow
-    # y_train = [train_dataset[i][3] for i in range(len(train_dataset))]
+    x_train = train_vectorizer.bow
+    y_train = [train_dataset[i][3] for i in range(len(train_dataset))]
 
 
 
